09cc7f62d3e86384cc5ae218c02a0df65b13bea2
tonyihvn <anthonynwokoma1@gmail.com>
2020-02-24 16:41:12 +0100

    First Page

--- controllers/seedcareToNMRS.php
+++ controllers/seedcareToNMRS.php
@@ -2,6 +2,9 @@
 
 //get connection parameters
 // require 'config/database.php';
+
+// This is the main class that will be used to do the migration proper.
+// Several methods will be created here inorder to perform the migration
 class seedcareToNMRS {
     private $facilityName = "";
 	private $datimId = "";
@@ -9,10 +12,6 @@ class seedcareToNMRS {
 	function __construct( $facilityName, $datimId ) {
 		$this->facilityName = $facilityName;
 		$this->datimId = $datimId;
-	}
- 
-	function getFileType() {
-		return $this->name;
 	}
  
 }
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <title>Title Page</title>
 
         <!-- Bootstrap CSS -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,26 +18,39 @@
         <![endif]-->
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand">Brand</a>
-            <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="my-nav" class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Item 1 <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Item 2</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+       <nav class="navbar navbar-expand-md navbar-light bg-light">
+           <a class="navbar-brand" href="#">SEEDCARE2NMRS</a>
+           <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+               aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="collapsibleNavId">
+               <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                   <li class="nav-item active">
+                       <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="#">HOW TO</a>
+                   </li>
+                   <li class="nav-item dropdown">
+                       <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                       <div class="dropdown-menu" aria-labelledby="dropdownId">
+                           <a class="dropdown-item" href="#">SET UP THE MIGRATOR</a>
+                           <a class="dropdown-item" href="#">MIGRATE TO NMRS</a>
+                       </div>
+                   </li>
+               </ul>
+               <form class="form-inline my-2 my-lg-0">
+                   <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                   <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+               </form>
+           </div>
+       </nav>
+        
 
         <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="assets/js/jquery-3.4.1.min.js"></script>
         <!-- Bootstrap JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
     </body>
 </html>
